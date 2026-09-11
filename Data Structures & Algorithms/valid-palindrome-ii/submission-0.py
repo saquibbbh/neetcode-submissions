39c7654401854e542379701c6ed30da9c380dc82
saquibbbh <saquibh923@gmail.com>
2026-09-11 19:49:53 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        if s == s[::-1]:
+            return True
+
+        for i in range(len(s)):
+            new = s[:i] + s[i+1:]
+
+            if new == new[::-1]:
+                return True
+
+        return False
